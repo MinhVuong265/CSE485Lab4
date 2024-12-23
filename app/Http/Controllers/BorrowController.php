b97785b1835f6e0efdf823ffc4ec52905b55a919
Minh Vương <tmvuong2k4@gmail.com>
2024-12-23 13:09:53 +0700

    update borrow

--- app/Http/Controllers/BorrowController.php
+++ app/Http/Controllers/BorrowController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Borrow;
+use App\Models\Reader;
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BorrowController extends Controller
@@ -11,7 +14,8 @@ class BorrowController extends Controller
      */
     public function index()
     {
-        //
+        $borrows = Borrow::with('reader', 'book')->get();
+        return view('borrow.index', compact('borrows'));
     }
 
     /**
@@ -19,7 +23,9 @@ class BorrowController extends Controller
      */
     public function create()
     {
-        //
+        $readers = Reader::all();
+        $books = Book::where('quantity', '>', 0)->get();
+        return view('borrow.create', compact('readers', 'books'));
     }
 
     /**
@@ -27,15 +33,39 @@ class BorrowController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $data = $request->validate([
+            'reader_id' => 'required|exists:readers,id',
+            'book_id' => 'required|exists:books,id',
+            'borrow_date' => 'required|date',
+            'return_date' => 'required|date'
+        ]);
+
+        $book = Book::find($data['book_id']);
+        if($book->quantity < 1){
+            return back()->with('error', "Sách đã hết");
+        }
+
+        $existingBorrow = Borrow::where('reader_id', $data['reader_id'])->where('book_id', $book->id)->where('status', 0)->first();
+
+        if($existingBorrow){
+            return back()->with('error', "Độc giả đang mượn sách này");
+        }
+
+        Borrow::create($data);
+
+        $book->decrement('quantity');
+
+        return redirect()->route('borrow.index')->with('success', 'Mượn sách thành công');
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function showHistory(Reader $reader)
     {
         //
+        $borrows = $reader->borrow()->with('book')->get();
+        return view('reader.history', compact('reader', 'borrows'));
     }
 
     /**
@@ -57,8 +87,15 @@ class BorrowController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function returnBook(Borrow $borrow)
     {
-        //
+        $borrow->update([
+            'return_date' => now(),
+            'status' => 1
+        ]);
+
+        $borrow->book->increment('quantity');
+
+        return back()->with('success', 'Trả sách thành công');
     }
 }
--- app/Models/Borrow.php
+++ app/Models/Borrow.php
@@ -9,4 +9,14 @@ class Borrow extends Model
 {
     use HasFactory;
     //
+    protected $fillable = ['reader_id', 'book_id', 'borrow_date', 'return_date', 'status'];
+    public function book()
+    {
+        return $this->belongsTo(Book::class);
+    }
+
+    public function reader()
+    {
+        return $this->belongsTo(Reader::class);
+    }
 }
